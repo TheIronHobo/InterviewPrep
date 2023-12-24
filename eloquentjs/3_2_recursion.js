3cf2de4282b497dd6eb75ed4c1499258fc9b7fc0
Aaron <aarongdabney@gmail.com>
2023-12-24 11:42:58 -0800

    misc changes

--- eloquentjs/3_2_recursion.js
+++ eloquentjs/3_2_recursion.js
@@ -1,23 +1,29 @@
-/**Tests if input digit is even and returns boolean*/
+/**
+ * Tests if input digit is even and returns boolean
+ * @param {*} input 
+ * @returns 
+ */
 function isEven(input) {
     if (isNaN(input)) {
         throw "isEven input cannot be NaN";
-        return;
     }
     if (!Number.isInteger(input)) {
         throw "isEven input must be an integer";
-        return;
     }
+
     if (input < 0) {
         input *= -1;
     }
-    if (input == 1) {
+
+    if (input === 1) {
         return false;
     }
-    if (input == 0) {
+    else if (input === 0) {
         return true;
     }
-    return(isEven(input-2));
+    else {
+        return isEven(input-2);
+    }
 }
 
 let testSpan = 10;
