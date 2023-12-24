d9312af24e71ed0595379a472082437e26a9087f
Aaron <aarongdabney@gmail.com>
2023-12-24 09:06:38 -0800

    finished recursion first draft

---
+++ eloquentjs/3_2_recursion.js
@@ -0,0 +1,26 @@
+function isEven(input) {
+    if (isNaN(input)) {
+        throw "isEven input cannot be NaN";
+        return;
+    }
+    if (!Number.isInteger(input)) {
+        throw "isEven input must be an integer";
+        return;
+    }
+    if (input < 0) {
+        input *= -1;
+    }
+    if (input == 1) {
+        return false;
+    }
+    if (input == 0) {
+        return true;
+    }
+    return(isEven(input-2));
+}
+
+let testSpan = 10;
+
+for (let i = -testSpan; i <= testSpan; i++) {
+    console.log(i+': ' + isEven(i));
+}
