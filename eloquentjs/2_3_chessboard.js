e866c1b3a1fda0e8022157ea7d880404697f4603
Aaron <aarongdabney@gmail.com>
2023-12-24 11:20:13 -0800

    miscellaneous fixes

--- eloquentjs/2_3_chessboard.js
+++ eloquentjs/2_3_chessboard.js
@@ -1,17 +1,19 @@
-function drawChessboard(width, height = width) {
+function drawChessboard(width, height) {
+    if (height === undefined) {
+        height = width;
+    }
     if (isNaN(width) || isNaN(height)) {
         throw "Chessboard dimensions cannot be NaN";
-        return;
     }
     if ((width <= 0) || (height <= 0)) {
         throw "Chessboard dimensions cannot be zero or less";
-        return;
     }
+
     for (let i = 0; i < height; i++) {
         let output = '';
         for (let j = 0; j < width; j++) {
-            let reverseRowPattern = (j%2 != i%2);
-            output += reverseRowPattern ? '⬜' : '⬛';
+            let reverseRowPattern = (j%2 !== i%2);
+            output += reverseRowPattern ? '⬛' : '⬜';
         }
         console.log(output);
     }
