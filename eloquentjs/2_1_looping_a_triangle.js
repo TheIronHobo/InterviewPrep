f4c24f2848c754ce3fe4701f4c922755415f211d
Aaron <aarongdabney@gmail.com>
2023-12-23 21:39:47 -0800

    collapsed alternate implentations to single fine

---
+++ eloquentjs/2_1_looping_a_triangle.js
@@ -0,0 +1,43 @@
+/**Draws a right triangle utilizing the += operator*/
+function drawTriangle(height) { 
+    if (Number.isNaN(height)) return;
+    if (height <= 0) return;
+    let tri = '';
+    for (let i = 0; i <= height; i++) {
+        for (let j = 0; j < i; j++) {
+            tri += "#";
+        }
+        tri += "\n";
+    }
+    console.log(tri);
+}
+
+/**Draws a right triangle utilizing array join method*/
+function drawTriangleArrayJoin(height) { 
+    if (Number.isNaN(height)) return;
+    if (height <= 0) return;
+    let tri = [];
+    for (let i = 0; i <= height; i++) {
+        for (let j = 0; j < i; j++) {
+            tri.push("#");
+        }
+        tri.push("\n");
+    }
+    console.log(tri.join(''));
+}
+
+/**Draws a right triangle utilizing combination of array join and repeat function*/
+function drawTriangleRepeat(height) { 
+    if (Number.isNaN(height)) return;
+    if (height <= 0) return;
+    let tri = [];
+    for (let i = 0; i <= height; i++) {
+        tri.push(('#').repeat(i))
+        tri.push("\n");
+    }
+    console.log(tri.join(''));
+}
+
+drawTriangle(7);
+drawTriangleArrayJoin(7);
+drawTriangleRepeat(7);
