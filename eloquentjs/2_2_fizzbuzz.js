4f58b6ec954ba6c3c4cfd422e843e3d4d9432270
Aaron <aarongdabney@gmail.com>
2023-12-23 21:56:50 -0800

    Corrected output error and added input screening

--- eloquentjs/2_2_fizzbuzz.js
+++ eloquentjs/2_2_fizzbuzz.js
@@ -1,9 +1,25 @@
-function FizzBuzz(iter){
-    for(let i = 1; i <= iter; i++){
+/** Performs the FizzBuzz operation up to and including iter*/
+function FizzBuzz(iter) {
+    if (Number.isNaN(iter)) {
+        throw "FizzBuzz input is NaN"
+        return;
+    }
+    if (iter<=0) {
+        throw "FizzBuzz input is less than zero";
+        return;
+    }
+    for (let i = 1; i <= iter; i++) {
         let output = '';
-        if(i%3==0) output+="Fizz";
-        if(i%5==0) output+="Buzz";
-        if(output!='') console.log(i + ': '+ output);
+        if (i%3 === 0) {
+            output += "Fizz";
+        }
+        if (i%5 === 0) {
+            output += "Buzz";
+        }
+        if (!output) {
+            output += i;
+        }
+        console.log(output);
     }
 }
 
