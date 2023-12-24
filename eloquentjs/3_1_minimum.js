a27a14dc87b2e7234c5bdc369ffaee0b75da4200
Aaron <aarongdabney@gmail.com>
2023-12-24 08:40:52 -0800

    added more test cases

--- eloquentjs/3_1_minimum.js
+++ eloquentjs/3_1_minimum.js
@@ -1,6 +1,6 @@
 function minimum(a,b) {
     if (isNaN(a) || isNaN(b)) {
-        throw "Input cannot be NaN";
+        throw "Minimum input cannot be NaN";
         return;
     }
     if (a < b) {
@@ -15,3 +15,5 @@ function minimum(a,b) {
 console.log(minimum(-5,10));
 console.log(minimum(500,4.6));
 console.log(minimum(1,1));
+console.log(minimum(3.141,-Infinity));
+console.log(minimum('apple',Infinity));
