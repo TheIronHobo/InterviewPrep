494069c0c3402f8f759e1df398c3ab50c5c9065c
Aaron <aarongdabney@gmail.com>
2023-12-23 22:41:03 -0800

    input sanitization, code reformatting, clarity improvements

--- eloquentjs/2_3_chessboard.js
+++ eloquentjs/2_3_chessboard.js
@@ -1,11 +1,20 @@
-function Chessboard(width, height){
-    for(let i = 0; i <height; i++){
-        let output='';
-        for(let j=0;j<width;j++){
-            output += (j%2!=i%2) ? '#': ' ';
+function Chessboard(width, height = width) {
+    if (isNaN(width) || isNaN(height)) {
+        throw "Chessboard dimensions cannot be NaN";
+        return;
+    }
+    if ((width <= 0) || (height <= 0)) {
+        throw "Chessboard dimensions cannot be zero or less";
+        return;
+    }
+    for (let i = 0; i < height; i++) {
+        let output = '';
+        for (let j = 0; j < width; j++) {
+            let reverseRowPattern = (j%2 != i%2);
+            output += reverseRowPattern ? '#' : ' ';
         }
         console.log(output);
     }
 }
 
-Chessboard(8, 8);
+Chessboard(8);
