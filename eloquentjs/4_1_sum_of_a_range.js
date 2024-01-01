ede8d5d8e5cf83c5e660c706349e1f2c187f2d66
Aaron <aarongdabney@gmail.com>
2024-01-01 09:46:05 -0800

    refactor complete

--- eloquentjs/4_1_sum_of_a_range.js
+++ eloquentjs/4_1_sum_of_a_range.js
@@ -1,8 +1,6 @@
 /**
  * Returns the range of numbers from between start-end by incrementing by the value of step
- * The sign of step is not taken into account, just the absolute value
- * If only two parameters are provided the function defaults to a step size of 1
- * If the end paramater is smaller than the start paramater the function assumes a negative step iteration
+ * If only two parameters are provided the function defaults to a step size of positive 1
  * @param {*} a 
  * @param {*} b 
  * @param {*} step 
@@ -21,20 +19,35 @@ function range(start, end, step = 1) {
     if (step === 0) {
         throw "step cannot be zero";
     }
-
-    step = Math.abs(step);
-    let reverse = (end < start) ? -1 : 1;
-
-    output = [];
-    for (let i = start; (i*reverse) <= (reverse*end); i += (reverse*step)) {
-        output.push(i);
+    if (start < end && Math.sign(step) === -1) {
+        return [];
     }
 
+    let reverseLoopTerminationCondition = 1;
+    if (end < start) {
+        if (Math.sign(step) === 1) {
+            return [];
+        }
+        reverseLoopTerminationCondition = -1;
+    }
+
+    output = [];
+    for (let i = start; i*reverseLoopTerminationCondition <= end*reverseLoopTerminationCondition; i += step) {
+        output.push(i);
+    }
     return output;
 }
 
-
+/**
+ * Returns the sum of an array of input numbers
+ * @param {*} input 
+ * @returns 
+ */
 function sum(input) {
+    if (!Array.isArray(input)) {
+        throw "invalid input";
+    }
+
     let total = 0;
     for (let i = 0; i < input.length; i++) {
         total += input[i];
@@ -47,33 +60,27 @@ function sum(input) {
     return total;
 }
 
-console.log(sum(range(1, 5)));   
-console.log((range(1, 5)));      
-console.log((range(5, 1)));       
+console.log("\n");
+console.log("Range Functon Testing");
+console.log("Lesser Start | Bigger End | No Step       " + range(1,5));     // [1,2,3,4,5]
+console.log("Lesser Start | Bigger End | Positive Step " + range(1,5,1));   // [1,2,3,4,5]
+console.log("Lesser Start | Bigger End | Negative Step " + range(1,5,-1));  // []
+console.log("Bigger Start | Lesser End | No Step       " + range(5,1));     // []
+console.log("Bigger Start | Lesser End | Positive Step " + range(5,1,1));   // []
+console.log("Bigger Start | Lesser End | Negative Step " + range(5,1,-1));  // [5,4,3,2,1]
+console.log("\n");
 
+console.log("Sum Functon Testing");
+console.log(sum(range(1,10))); //55
+console.log("\n");
 
-console.log('\n');
-console.log(sum(range(-1, 4)));   
-console.log((range(-1, 4)));     
-console.log((range(4, -1)));       
-console.log((range(-1, 4 ,-1)));   
-console.log('\n');
+// console.log("Range Functon Error Conditions");
+// console.log("NaN parameter" + range('a',5));                 // Error
+// console.log("Infinity parameter" + range(1,Infinity));       // Error
+// console.log("Zero Step parameter" + range(1,5,0));           // Error
+// console.log("\n");
 
-console.log((range(1, 5, 2)));
-console.log((range(1, 5, -2)));     
-console.log('\n');
-
-console.log((range(7, 9, 0.5)));        // [ 7, 7.5, 8, 8.5, 9 ]
-console.log((range(9, 7, -0.5)));       // [ 9, 8.5, 8, 7.5, 7 ]
-console.log('\n');
-
-console.log((range(2, 5, 1)));          // [ 2, 3, 5, 5 ]
-console.log((range(5, 2, 1)));          // [ 2, 3, 5, 5 ]
-console.log((range(2, 5, -1)));         // [ 5, 5, 3, 2 ] 
-console.log((range(5, 2, -1)));         // [ 5, 5, 3, 2 ] 
-console.log('\n');
-
-// console.log(sum(range('more cowbell',9)));  // Error
-// console.log(sum(range(9,'more cowbell')));  // Error
-// console.log((range(5, 2, 0)));              // Error
-//console.log(sum(range(5)));                   // Invalid Input
+// console.log("Sum Functon Error Conditions");
+// console.log(sum('orange'));                                  // Error
+// console.log(sum(['a',2,3,4,5]));                             // Error
+// console.log("\n");
