9a9f9941ff05497e90628ac76d3d8b4210613f6f
Aaron <aarongdabney@gmail.com>
2024-01-11 19:07:17 -0800

    misc refactoring

--- eloquentjs/4_3_list.js
+++ eloquentjs/4_3_list.js
@@ -3,25 +3,29 @@
  * @param {*} inputArray 
  * @returns 
  */
-function arrayToList(inputArray) { 
-    if (!Array.isArray(inputArray)) { 
-        throw "input must be array";
-    }
-    if (inputArray.length === 0) {
-        return {value: null, rest: null};
-    }
-
-    const maxIndex = inputArray.length - 1;
-
-    function innerArrayToList(currentIndex, list = null) {
-        if (currentIndex < 0) {
-            return list;
+ function arrayToList(inputArray) { 
+        if (!Array.isArray(inputArray)) { 
+            throw "input must be array";
         }
+    
+        const maxIndex = inputArray.length - 1;
+    
+        function innerArrayToList(currentIndex, list = {value: null, rest: null}) {
+            const arrayElement = inputArray[currentIndex];
 
-        const arrayElement = inputArray[currentIndex];
+            if (arrayElement === undefined) {
+                return list;
+            }
 
-        return innerArrayToList(currentIndex - 1, {value: arrayElement, rest: list});
-    }
+            if (list.value === null) {
+                list.value = arrayElement;
+            } else {
+                list = {value: arrayElement, rest: list};
+            }
+
+            currentIndex--;
+            return innerArrayToList(currentIndex, list);
+        }
 
     return innerArrayToList(maxIndex);
 }
@@ -35,18 +39,17 @@ function arrayToListIterative(inputArray) {
     if (!Array.isArray(inputArray)) { 
         throw "input must be array";
     }
-    if (inputArray.length === 0) {
-        return {value: null, rest: null};
-    }
 
-    let listAcc = null;
+    let listAcc = {value: null, rest: null};
 
     for (let i = inputArray.length - 1; i >= 0; i--) {
-        const element = inputArray[i];
-        listAcc = {
-            value: element,
-            rest: listAcc
-        };
+        const arrayElement = inputArray[i];
+
+        if (listAcc.value === null) {
+            listAcc.value = arrayElement;
+        } else {
+            listAcc = {value: arrayElement, rest: listAcc};
+        }
     }
 
     return listAcc;
@@ -63,15 +66,15 @@ function listToArray(inputList) {
     }
 
     function innerListToArray(list, arrayAccumulator = []) {
-        if(list.value !== null) {
+        if (list === null) {
+            return arrayAccumulator;
+        }
+        
+        if (list.value) {
             arrayAccumulator.push(list.value);
         }
 
-        if (list.rest === null) {
-            return arrayAccumulator;
-        } else {
-            return innerListToArray(list.rest, arrayAccumulator);
-        }
+        return innerListToArray(list.rest, arrayAccumulator);
     }
 
     return innerListToArray(inputList);
@@ -86,15 +89,14 @@ function listToArrayIterative(inputList) {
     if (!isList(inputList)) {
         throw "input must be list data structure";
     }
-    if (inputList.value === null) {
-        return [];
-    }
 
     let acc = [];
-
     let listBuffer = inputList;
+
     while (listBuffer !== null) {
-        acc.push(listBuffer.value)
+        if (listBuffer.value) {
+            acc.push(listBuffer.value);
+        }
         listBuffer = listBuffer.rest;
     }
 
@@ -131,13 +133,14 @@ function nth(inputList, targetIndex) {
     if (isNaN(targetIndex) || targetIndex < 0 || Math.abs(targetIndex) === Infinity) {
         throw "invalid target index";
     }
-    if (targetIndex > getListLength(inputList) - 1) {
-        throw "invalid target index";
-    }
 
     listBuffer = inputList;
     for (let i = 0; i < targetIndex; i++) {
         listBuffer = listBuffer.rest;
+
+        if (listBuffer === null) {
+            throw "invalid target index";
+        }
     }
 
     return listBuffer.value;
@@ -167,13 +170,12 @@ function getListLength(inputList) {
     }
 
     function innerListLength(list, listLength = 0) {
-        if (list.value !== null) {
-            listLength++;
-        }
-        if (!list.rest) {
+        if (!list || (list.value === null)) {
             return listLength;
+        } else {
+            listLength++;
+            return innerListLength(list.rest, listLength);
         }
-        return innerListLength(list.rest, listLength);
     }
 
     return innerListLength(inputList);
@@ -195,7 +197,7 @@ console.log("Iteration| testArray to list: "    +   JSON.stringify(arrayToListIt
 console.log("Recursion| testList to array: "     +   JSON.stringify(listToArray(testList)));
 console.log("Iteration| testList to array: "     +   JSON.stringify(listToArrayIterative(testList)));
 console.log('\n');
-console.log("Empty/Null Testing");
+console.log("Empty/Null/Undefined Testing");
 console.log("Recursion| emptyArray to list: "    +   JSON.stringify(arrayToList(emptyArray)));
 console.log("Iteration| emptyArray to list: "    +   JSON.stringify(arrayToListIterative(emptyArray)));
 console.log(`Recursion| emptyList to array: `     +   JSON.stringify(listToArray(emptyList)));
@@ -204,7 +206,8 @@ console.log('\n');
 console.log("Helper Function Testing");
 console.log(`testList with prepended element '☕' `    +   JSON.stringify(prepend(testList, '☕')));
 console.log(`Returning index 2 element from testList: `+   nth(testList, 2));
-console.log("Get testList Length "+ getListLength(testList));
+console.log("Get length of test list "+ getListLength(testList));
+console.log("Get length of empty list "+ getListLength(emptyList));
 console.log("Is testList a list? " + isList(testList));
 console.log(`Is 'mayonnaise' a list? ` + isList('mayonnaise'));
 
