3d02217d3c410e3239b11815b5688511af1ab1fe
Aaron <aarongdabney@gmail.com>
2024-01-12 20:41:58 -0800

    array based testing

--- eloquentjs/5_1_flattening.js
+++ eloquentjs/5_1_flattening.js
@@ -1,20 +1,21 @@
-let singleArray = [['a','b','c']];
-let doubleArray = [['a','b','c'], [1, 2, 3]];
-let tripleArray = [['a','b','c'], [1, 2, 3], ["🌒", "🌓", "🌔"]];
+const testCases = [
+    [['a', 'b', 'c']],
+    [['a', 'b', 'c'], [1, 2, 3]],
+    [['a', 'b', 'c'], [1, 2, 3], ["🌒", "🌓", "🌔"]],
+    [[]],
+    [[], [], []],
+];
 
-let flattenedSingleArray = singleArray.reduce((acc, currentArray) => acc.concat(currentArray));
-let flattenedDoubleArray = doubleArray.reduce((acc, currentArray) => acc.concat(currentArray));
-let flattenedTripleArray = tripleArray.reduce((acc, currentArray) => acc.concat(currentArray));
+const errorCases = [
+    [],
+    'legumes',
+    undefined,
+    3,
+    Infinity,
+    null
+];
 
-console.log('\n');
-console.log("flattenedSingleArray   | " + JSON.stringify(flattenedSingleArray));
-console.log("flattenedDoubleArray   | " + JSON.stringify(flattenedDoubleArray));
-console.log("flattenedTripleArray   | " + JSON.stringify(flattenedTripleArray));
-console.log('\n');
-
-let doubleNestedEmptyArray = [[]];
-let flattenedDoubleNestedEmptyArray = doubleNestedEmptyArray.reduce((acc, currentArray) => acc.concat(currentArray));
-console.log("dobuleNestedEmptyArray | " + JSON.stringify(flattenedDoubleNestedEmptyArray));
-
-let emptyArray = [];
-let flattenedEmptyArray = emptyArray.reduce((acc, currentArray) => acc.concat(currentArray)); // Error
+for (let i = 0; i < testCases.length; i++) {
+    flattenedElement = testCases[i].reduce((acc, currentArray) => acc.concat(currentArray))
+    console.log('\n' + JSON.stringify(testCases[i]) + " --> " + JSON.stringify(flattenedElement));
+}
