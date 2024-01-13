91f077c084649333bba30545f3f05e054292f5a0
Aaron <aarongdabney@gmail.com>
2024-01-12 21:51:14 -0800

    misc changes

--- eloquentjs/5_2_your_own_loop.js
+++ eloquentjs/5_2_your_own_loop.js
@@ -5,9 +5,11 @@ function loop(value, testFunction, updateFunction, bodyFunction) {
     }
 }
 
-const addOne = x => x + 1;
+const addN = n => (x => x + n);
 const fireLaserCannonNTimes = n => console.log(" Pew! ".repeat(n));
 const lessThanN = n => (x => x < n);
+
+const addOne = addN(1);
 const lessThanSix = lessThanN(6);
 
 loop(0, lessThanSix, addOne, fireLaserCannonNTimes);
