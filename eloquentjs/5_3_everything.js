f282b3368e7504c439aa3017ec4cf86f0a484676
Aaron <aarongdabney@gmail.com>
2024-01-15 21:39:42 -0800

    misc changes

--- eloquentjs/5_3_everything.js
+++ eloquentjs/5_3_everything.js
@@ -40,49 +40,42 @@ const testFunctionDictionary = {
 }
 
 console.log("Testing Everything Some...\n");
-
-let passesAllTests = true;
-
-for (let testArray of testCases) {
-    for (const [name, func] of Object.entries(testFunctionDictionary)) {
-        const textDescription = "Does everything in the array " + JSON.stringify(testArray) + " pass the " + name + " test?";
-        const spacingDiff = 90 - textDescription.length;
-
-        const everythingResult = everythingSome(testArray, func)
-        console.log(textDescription + " ".repeat(spacingDiff) +" | " + everythingResult); 
-
-        if (everythingResult !== testArray.every(func)) {
-            passesAllTests = false;
-        }
-    }
-}
-
-if (passesAllTests) {
-    console.log("Everything Some passes all tests! ✔️✨\n")
-} else {
-    console.log("Everything Some has failed some tests...");
-}
+testEverythingFunction(everythingSome, testCases, testFunctionDictionary);
 
 console.log("Testing Everything Loop...\n");
+testEverythingFunction(everythingLoop, testCases, testFunctionDictionary);
 
-passesAllTests = true;
+function testEverythingFunction(everythingFunc, testCases, testFunctionDictionary) {
+    let failureStatements = [];
+    
+    for (let testArray of testCases) {
+        for (const [name, func] of Object.entries(testFunctionDictionary)) {
+            const everythingResult = everythingFunc(testArray, func)
 
-for (let testArray of testCases) {
-    for (const [name, func] of Object.entries(testFunctionDictionary)) {
-        const textDescription = "Does everything in the array " + JSON.stringify(testArray) + " pass the " + name + " test?";
-        const spacingDiff = 90 - textDescription.length;
-
-        const everythingResult = everythingLoop(testArray, func)
-        console.log(textDescription + " ".repeat(spacingDiff) +" | " + everythingResult); 
-
-        if (everythingResult !== testArray.every(func)) {
-            passesAllTests = false;
+            if (everythingResult !== testArray.every(func)) {
+                failureStatements.push(`Failure #${failureStatements.length}: everything(${JSON.stringify(testArray)}, ${name} === ${everythingResult})`);
+            }
+        }
+    }
+    if (failureStatements.length === 0) {
+        console.log("Everything function passed all tests! ✔️✨\n")
+    } else {
+        console.log("Everything function has failed the following tests...");
+        for (let statement of failureStatements) {
+            console.log(statement);
         }
     }
 }
 
-if (passesAllTests) {
-    console.log("Everything Loop passes all tests! ✔️✨\n")
-} else {
-    console.log("Everything Loop has failed some tests...");
-}
+// The following was used to test the tester!
+// function everythingWrong(inputArray, testFunction) { 
+//     for (let element of inputArray) {
+//         if (!testFunction(element)) {
+//             return true
+//         }
+//     }
+//     return false;
+// }
+
+// console.log("Testing Everything Wrong...\n");
+// testEverythingFunction(everythingWrong, testCases, testFunctionDictionary);
