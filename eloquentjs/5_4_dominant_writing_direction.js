c4c06dc6a8650a6b02ad49fde3cef3340fdd2607
unknown <aarongdabney@gmail.com>
2024-01-17 08:35:57 -0800

    creating test system wip

--- eloquentjs/5_4_dominant_writing_direction.js
+++ eloquentjs/5_4_dominant_writing_direction.js
@@ -46,7 +46,7 @@ function countBy(items, groupName) {
     return counts;
 }
 
-console.log(dominantWritingDirection("ba亣gelﺏ"));
+//console.log(dominantWritingDirection("ba亣gelﺏ"));
 
 
 // I should build a function that generates a random number somewhere in the entire unicode range
@@ -54,3 +54,28 @@ console.log(dominantWritingDirection("ba亣gelﺏ"));
 //we use a more ineffecient method to verify the veracity of the unicode characters?
 
 //get random sampling of different objects
+
+let testObject = {
+    directions: ['ltr', 'rtl'],
+    text: "ba亣gelﺏ",
+}
+
+// we generate 
+
+function testDominantWritingDirectionFunction(dominantWritingDirection) {
+
+    //get random script
+    //save writing direction, deposit into test object
+    //get random range within ranges of script
+    //get random number within the range
+    const randomIntegerInRange = (start, end) => Math.floor((end - start)*Math.random()) + start; // Intentionally end exclusive to match unicode range standards
+    
+    for(let i = 0; i < 12; i++) {
+        console.log(randomIntegerInRange(0,5));
+    }
+    //deposit the character code as string into test object
+    //give the dominant writing system the string and check its result matches the test object
+    //if not provide failure diagnostics
+}
+
+testDominantWritingDirectionFunction(dominantWritingDirection);
