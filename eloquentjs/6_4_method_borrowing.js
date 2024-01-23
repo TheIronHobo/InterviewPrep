bfa2f7187040482e949a1b46376a37d707e9b2e8
unknown <aarongdabney@gmail.com>
2024-01-23 10:04:05 -0800

    first draft

---
+++ eloquentjs/6_4_method_borrowing.js
@@ -0,0 +1,20 @@
+class Lemur {
+    constructor(name, furType) {
+        this.name = name;
+        this.fur = furType;
+    }
+}
+
+let mikey = new Lemur("Michael", "soft");
+
+mikey.weight = 5000;
+
+console.log(mikey.hasOwnProperty("weight")); // true
+
+mikey.hasOwnProperty = 'I peddle pristine properties';
+
+const hasOwnPropertySymbol = Symbol("hasOwnProperty");
+Lemur.prototype[hasOwnPropertySymbol] = Object.prototype.hasOwnProperty;
+
+console.log(mikey[hasOwnPropertySymbol]("weight")); // true
+console.log(mikey[hasOwnPropertySymbol]("morals")); // false
