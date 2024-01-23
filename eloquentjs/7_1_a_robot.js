483680d19f48388f72bc8f50d17e44143ea0f604
unknown <aarongdabney@gmail.com>
2024-01-23 11:34:43 -0800

    building 3

--- eloquentjs/7_1_a_robot.js
+++ eloquentjs/7_1_a_robot.js
@@ -48,6 +48,19 @@ class VillageState {
             }).filter(p => p.place != p.address);
             return new VillageState(destination, parcels);
         }
-        
+
+    }
+}
+
+function runRobot(state, robot, memory) {
+    for (let turn = 0;; turn++) {
+        if (state.parcels.length == 0) {
+            console.log(`Done in ${turn} turns`);
+            break;
+        }
+        let action = robot(state, memory);
+        state =  state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
     }
 }
