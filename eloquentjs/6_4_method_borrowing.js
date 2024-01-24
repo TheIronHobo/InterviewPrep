cdf0e6ba5ea789b291cd416c5ce8554831fd12a5
unknown <aarongdabney@gmail.com>
2024-01-24 10:00:33 -0800

    call method

--- eloquentjs/6_4_method_borrowing.js
+++ eloquentjs/6_4_method_borrowing.js
@@ -1,26 +1,11 @@
-class Lemur {
-    constructor(name, furType) {
-        this.name = name;
-        this.fur = furType;
-    }
-}
-
-let mikey = new Lemur("Michael", "soft");
-
-mikey.weight = 5000;
-
-console.log(mikey.hasOwnProperty("weight")); // true
-
-mikey.hasOwnProperty = 'I peddle pristine properties';
-
 const hasOwnPropertySymbol = Symbol("hasOwnProperty");
-Lemur.prototype[hasOwnPropertySymbol] = Object.prototype.hasOwnProperty;
-
-console.log(mikey[hasOwnPropertySymbol]("weight")); // true
-console.log(mikey[hasOwnPropertySymbol]("morals")); // false
 
 let map = {one: true, two: true, hasOwnProperty: true};
 
-
 Object.prototype[hasOwnPropertySymbol] = Object.hasOwnProperty;
 console.log(map[hasOwnPropertySymbol]("one")); // true
+
+// Alternate solution using the .call method
+console.log(Object.hasOwnProperty.call(map, "one")) // true
+console.log(Object.hasOwnProperty.call(map, "two")) // true
+console.log(Object.hasOwnProperty.call(map, "goo")) // false
