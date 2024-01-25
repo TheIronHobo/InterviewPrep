871b8b19de51330342c6af2c62069dd9834de7b7
Aaron <aarongdabney@gmail.com>
2024-01-24 22:11:26 -0800

    more robots

--- eloquentjs/7_1_a_robot.js
+++ eloquentjs/7_1_a_robot.js
@@ -6,8 +6,14 @@ const roads = [
     "Grete's House-Shop",          "Marketplace-Farm",
     "Marketplace-Post Office",     "Marketplace-Shop",
     "Marketplace-Town Hall",       "Shop-Town Hall"
-  ];
-  
+];
+
+const mailRoute = [
+    "Alice's House", "Cabin", "Alice's House", "Bob's House",
+    "Town Hall", "Daria's House", "Ernie's House",
+    "Grete's House", "Shop", "Grete's House", "Farm",
+    "Marketplace", "Post Office"
+];  
 
 function buildGraph(edges) {
     let graph = Object.create(null);
@@ -24,6 +30,7 @@ function buildGraph(edges) {
         addEdge(from, to);
         addEdge(to, from);
     }
+
     return graph;
 }
 
@@ -61,71 +68,43 @@ VillageState.random = function(parcelCount = 5) {
 }
 
 function runRobot(state, robot, memory) {
-    for (let turn = 0;; turn++) {
+    const maxIter = 500;
+    for (let turn = 0; turn < maxIter; turn++) {
         if (state.parcels.length == 0) {
-            console.log(`Done in ${turn} turns`);
-            break;
+            return turn;
         }
         let action = robot(state, memory);
         state =  state.move(action.direction);
         memory = action.memory;
-        console.log(`Moved to ${action.direction}`);
     }
+    throw `${robot.name} HAS EXCEED ${maxIter} ITERATIONS`;
 }
 
-function randomRobot(state) {
-    return {direction: randomPick(roadGraph[state.place])}
-}
+function robotComparison(robots, numTests) {
+    let results = [];
 
-function brutusBot(state,  memory = []) {
-    sucessfulRoutes = [];
-    console.log("State follow: ");
-    console.log(state);
+    const parcelCount = 5;
+    let tests = [];
 
-    if(memory.length === 0) {
-        breadthSearch(state, history = []);
+    for (let i = 0; i < numTests; i++) {
+        tests.push(new VillageState.random(5))
     }
 
-    function breadthSearch(internalState, history = [], depth = 0) {
+    for (robot of Object.keys(robots)) {
+        console.log(`Testing: ${robot}`);
+        let acc = 0;
 
-        history = [...history, internalState.place];
-        let nextPlaces = accessibleLocations(internalState);
-
-        if(depth >= 16) {
-            return;
+        const t0 = performance.now();
+        for (test of tests) {
+            acc += runRobot(test, robots[robot]);
         }
-        if(internalState.parcels.length === 0) {
-            sucessfulRoutes.push(history);
-        }
-
-        for (let i = 0; i < nextPlaces.length; i++) {
-
-           let newInternalState = internalState.move(nextPlaces[i]);
-
-           breadthSearch(newInternalState, history, depth + 1);
-        }
+        const t1 = performance.now();
+        const delta = t1-t0;
         
+        results.push({name: robot, averageSteps:acc/numTests, averageMilliseconds: delta/numTests});
     }
 
-    console.log("we having a good one eh?");
-    sucessfulRoutes.sort((a,b) => {
-        if(a.length > b.length) {
-            return 1;
-        } else if (a.length < b.length) {
-            return -1;
-        }
-        return 0;
-    });
-
-    console.log(`Best route: ` + sucessfulRoutes[0]);
-    console.log(`Best route length: ${sucessfulRoutes[0].length}`)
-    console.log(`Worst route: ` + sucessfulRoutes[sucessfulRoutes.length-1]);
-    throw "go home :)";
-
-    function accessibleLocations(state) {
-        return roadGraph[state.place]
-    }
-
+    return results;
 }
 
 function randomPick(array) {
@@ -133,11 +112,159 @@ function randomPick(array) {
     return array[choice];
 }
 
+function accessibleLocations(state, graph) {
+    return graph[state.place]
+}
+
+/**
+ * Pulled from E-JS with no alteration
+ * @param {*} graph 
+ * @param {*} from 
+ * @param {*} to 
+ * @returns 
+ */
+function findRoute(graph, from, to) {
+    let work = [{at: from, route: []}];
+    for (let i = 0; i < work.length; i++) {
+      let {at, route} = work[i];
+      for (let place of graph[at]) {
+        if (place == to) return route.concat(place);
+        if (!work.some(w => w.at == place)) {
+          work.push({at: place, route: route.concat(place)});
+        }
+      }
+    }
+}
+
+function randoBot(state) {
+    return {direction: randomPick(roadGraph[state.place])}
+}
+
+function brutusBot(state,  memory = {route: [], relevantLocations: []}) {
+    let bestRoute;
+
+    if(memory.relevantLocations.length === 0) {
+        for({place, address} of state.parcels) {
+            if (!memory.relevantLocations.includes(place)) {
+                memory.relevantLocations.push(place);
+            }
+            if (!memory.relevantLocations.includes(address)) {
+                memory.relevantLocations.push(address);
+            }
+        }
+    }
+
+    if (memory.route.length === 0) {
+        bruteSearch(state, history = []);
+        memory.route = bestRoute;
+        memory.route.slice(1);
+    }
+
+    function bruteSearch(internalState, history = [], depth = 0) {
+        history = [...history, internalState.place];
+        let nextPlaces = accessibleLocations(internalState, roadGraph);
+
+        if (depth >= roads.length*2 + 1) {
+            return;
+        }
+        if (bestRoute !== undefined && history.length >= bestRoute.length) {
+            return;
+        }
+        if (internalState.parcels.length === 0) {
+            bestRoute = history;
+            return;
+        }
+        if (history.length >= 3) {
+            let currentLocation = history[history.length-1]
+            let lastLocation = history[history.length-2];
+            let secondToLastLocation = history[history.length-3];
+
+            const uselessBacktracking = !memory.relevantLocations.includes(lastLocation) && (secondToLastLocation === currentLocation);
+            if (uselessBacktracking) {
+                return;
+            }
+        }
+
+        for (let i = 0; i < nextPlaces.length; i++) {
+           let newInternalState = internalState.move(nextPlaces[i]);
+           bruteSearch(newInternalState, history, depth + 1);
+        }
+    }
+
+    return {
+        direction: memory.route[0],
+        memory: {route: memory.route.slice(1), relevantLocations: memory.relevantLocations}
+    };
+}
+
+/**
+ * Bot pulled from E-JS with minimal alteration
+ * @param {*} state 
+ * @param {*} memory 
+ * @returns 
+ */
+function routeRobot(state, memory = []) {
+    if (memory.length === 0) {
+      memory = mailRoute;
+    }
+    return {direction: memory[0], memory: memory.slice(1)};
+}
+
+/**
+ * Bot pulled from E-JS with minimal alteration
+ * @param {*} param0 
+ * @param {*} route 
+ * @returns 
+ */
+function goalOrientedRobot({place, parcels}, route = []) {
+    if (route.length == 0) {
+      let parcel = parcels[0];
+      if (parcel.place != place) {
+        route = findRoute(roadGraph, place, parcel.place);
+      } else {
+        route = findRoute(roadGraph, place, parcel.address);
+      }
+    }
+    return {direction: route[0], memory: route.slice(1)};
+}
+
+function lovesHorsesRobot({place, parcels}, memory = {route: [], turnsWithoutSeeingHorse: Infinity}) {
+    if (place === "Farm") {
+        memory.turnsWithoutSeeingHorse = 0;
+    }
+    if (memory.turnsWithoutSeeingHorse > 0) {
+        memory.route = findRoute(roadGraph, place, "Farm");
+    } else if (memory.route.length == 0) {
+        let parcel = parcels[0];
+        if (parcel.place != place) {
+            memory.route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            memory.route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+    return {direction: memory.route[0], memory: {route: memory.route.slice(1), turnsWithoutSeeingHorse: memory.turnsWithoutSeeingHorse++}}
+}
+
 const roadGraph = buildGraph(roads);
 
-runRobot(VillageState.random(2), brutusBot);
-
-let exampleState =  {
-    place:"Alice's House",
-    parcels:[{place:"Alice's House",address:"Bob's House"}]
+let roboDictionary = {
+    randoBot: randoBot,
+    lovesHorsesRobot: lovesHorsesRobot,
+    brutusBot: brutusBot,
+    routeRobot: routeRobot,
+    goalOrientedRobot: goalOrientedRobot,
 }
+
+let results = robotComparison(roboDictionary, 50);
+
+results.sort((a,b)=>{
+    if(a.averageSteps > b.averageSteps) {
+        return 1;
+    } else if (a.averageSteps < b.averageSteps){
+        return -1;
+    }
+    return 0;
+});
+
+
+console.log(results);
