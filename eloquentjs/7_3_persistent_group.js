976bac7495fabc8b388a13fe044c982e4381e160
unknown <aarongdabney@gmail.com>
2024-01-31 14:14:31 -0800

    refactor

--- eloquentjs/7_3_persistent_group.js
+++ eloquentjs/7_3_persistent_group.js
@@ -1,79 +1,45 @@
-// https://eloquentjavascript.net/07_robot.html
 class PGroup {
-    constructor() {
-        this.group = [];
+    static empty = new PGroup();
+
+    constructor(group = []) {
+        this.group =  group;
     }
 
     has(item) {
-        return this.group.includes(item) ? true : false;
+        return this.group.includes(item);
     }
 
     add(item) {
         if (!this.has(item)) {
-            let output = new PGroup();
-
-            for (let i = 0; i < this.group.length; i++) {
-                output.group.push(this.group[i]);
-            }
-
-            output.group.push(item);
-
-            return output;
+            return new PGroup([...this.group, item]);
         } else {
             console.log(`Group already includes item '${item}!'`);
-            return this;
         }
     }
 
     delete(item) {
         if (this.has(item)) {
-            let output = new PGroup();
-
-            output.group = this.group.filter(j => j !== item)
-            
-            return output;
+            let cloneGroup = [...this.group];
+            cloneGroup.splice(cloneGroup.findIndex(j => j === item), 1);
+            return new PGroup(cloneGroup);
         } else {
             console.log(`Group doesn't have '${item}'!`);
-            return this;
         }
-
-    }
-
-    static from(inputArray) {
-        let output = new PGroup();
-
-        for (let i = 0; i < inputArray.length; i++) {
-            output = output.add(inputArray[i]);
-        }
-
-        return output;
     }
 
     display() {
-        console.log(this.group);
+        console.log(JSON.stringify(this.group));
     }
+
 }
 
-let test = [1, 2, 3, 4, 'a'];
+let a = PGroup.empty.add("a");
+let ab = a.add("b");
+let b = ab.delete("a");
 
-let testGroup = PGroup.from(test);
-
-console.log("\nOriginal");
-testGroup.display();
-
-console.log("\nAdding '6'");
-testGroup.add('6').display();
-
-console.log("\nAdding 6");
-testGroup.add(6).display();;
-
-console.log("\nDelete 2");
-testGroup.delete(2).display();;
-
-console.log("\nDelete 6");
-testGroup.delete(6).display();;
-
-console.log("\nDelete 't'");
-testGroup.delete('t').display();;
-
-console.log("\nTest complete");
+console.log(b.has("b"));
+// → true
+console.log(a.has("b"));
+// → false
+console.log(b.has("a"));
+// → false
