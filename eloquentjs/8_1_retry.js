1a6c553fe13eed419345b70a5b07739ae4b0bc87
unknown <aarongdabney@gmail.com>
2024-02-01 11:25:02 -0800

    refactor

--- eloquentjs/8_1_retry.js
+++ eloquentjs/8_1_retry.js
@@ -1,12 +1,37 @@
+class MultiplicationUnitFailure extends Error{};
+
 function primitiveMultiply(a, b) {
+    const innerPrimitiveMultiply = j => {
+        if (Math.random() < 0.80) {
+            throw new MultiplicationUnitFailure();
+        } else {
+            console.log("Multiplication sucessful!")
+            return a * b;
+        }
+    }
+
+    return MultiplicationUnitFailureErrorHanlder(innerPrimitiveMultiply);
+}
+
+function MultiplicationUnitFailureErrorHanlder(inputFunction) {
     for(;;) {
         try {
-            if (Math.random() > 0.8) {
-                throw new Error("MultiplicationUnitFailure");
+            return inputFunction();
+        } catch(e) {
+            if (e instanceof MultiplicationUnitFailure) {
+                console.log("Multiplication failed. Trying again..");
+            } else {
+                throw e;
             }
-            return a * b;
-        } catch {
-            return primitiveMultiply(a, b);
         }
     }
 }
+
+console.log(primitiveMultiply(3, 5));
+
+// Sample output: 
+// Multiplication failed. Trying again..
+// Multiplication failed. Trying again..
+// Multiplication failed. Trying again..
+// Multiplication sucessful!
+// 15
