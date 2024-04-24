7f65d4d64738c75deb2eeb65f351064123ff9a36
unknown <aarongdabney@gmail.com>
2024-04-23 20:47:08 -0700

    refactor

--- eloquentjs/11_2_building_promise_all.js
+++ eloquentjs/11_2_building_promise_all.js
@@ -1,16 +1,11 @@
-// takes in an array of promises and returns the result as a promise when all the promises have resolved
-// if any of the promises fail we should pass on the failure into the larger promise.
-// maybe we should give the first promise a callback to push its result to the result array and
-
 function Promise_all(promises) {
   return new Promise((resolve, reject) => {
-    output = [];
-
+  
     async function iteratePromises() {
-        for (let i = 0; i < promises.length; i++) {
+      output = [];
+        for (let promise of promises) {
           try {
-            let promiseResult = await promises[i];
-            output.push(promiseResult);
+            output.push(await promise);
           } catch (e) {
             reject(e);
           }
@@ -23,14 +18,14 @@ function Promise_all(promises) {
   });
 }
 
-// // Test code.
-// Promise_all([]).then(array => {
-//   console.log("This should be []:", array);
-// });
+// Test code.
+Promise_all([]).then(array => {
+  console.log("This should be []:", array);
+});
 
-// Promise_all([soon(1), soon(2), soon(3)]).then(array => {
-//   console.log("This should be [1, 2, 3]:", array);
-// });
+Promise_all([soon(1), soon(2), soon(3)]).then(array => {
+  console.log("This should be [1, 2, 3]:", array);
+});
 
 Promise_all([soon(1), Promise.reject("X"), soon(3)])
   .then(array => {
