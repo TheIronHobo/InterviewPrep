9e78b9c2b451b69356b2a70b9b0ea0513f5b277f
aarongdabney@gmail.com <aarongdabney@gmail.com>
2024-06-09 22:25:39 -0700

    first commit

---
+++ eloquentjs/14_1_build_a_table/index.js
@@ -0,0 +1,41 @@
+const MOUNTAINS = [
+    {name: "Kilimanjaro", height: 5895, place: "Tanzania"},
+    {name: "Everest", height: 8848, place: "Nepal"},
+    {name: "Mount Fuji", height: 3776, place: "Japan"},
+    {name: "Vaalserberg", height: 323, place: "Netherlands"},
+    {name: "Denali", height: 6168, place: "United States"},
+    {name: "Popocatepetl", height: 5465, place: "Mexico"},
+    {name: "Mont Blanc", height: 4808, place: "Italy/France"}
+];
+
+const mountainsTableElement = generateObjectTable(MOUNTAINS);
+document.getElementById("mountains").appendChild(mountainsTableElement);
+
+function generateObjectTable(inputArray) {
+    const table = document.createElement('table');
+
+    const headerRow = document.createElement('tr');
+    Object.getOwnPropertyNames(inputArray[0]).forEach(propertyName => {
+        const cell = document.createElement('th')
+        cell.innerHTML = propertyName;
+        headerRow.appendChild(cell);
+    });
+    table.appendChild(headerRow);
+
+    inputArray.forEach(entry => {
+        const row = document.createElement('tr');
+
+        for (const [key, value] of Object.entries(entry)) {
+            const cell = document.createElement('td');
+            cell.innerHTML = value;
+            if (!isNaN(value)) {
+                cell.style.textAlign = 'right';
+            }
+            row.appendChild(cell);
+        }
+
+        table.appendChild(row);
+    });
+
+    return table;
+}
