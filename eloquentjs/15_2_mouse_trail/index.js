d9e479e460f2c6e2e6d944b83bb34324158820a4
unknown <aarongdabney@gmail.com>
2024-06-20 10:02:00 -0700

    wip

--- eloquentjs/15_2_mouse_trail/index.js
+++ eloquentjs/15_2_mouse_trail/index.js
@@ -1,9 +1,19 @@
 import { Missile } from "./missile.js";
 import { Vector2 } from "./vector2.js";
 
+let divisions = 10;
+let squareSize = window.innerWidth/divisions;
 
+console.log(squareSize);
 
-let missile = new Missile(new Vector2(100, 100));
-
-missile.start();
-missile.update();
+for (let i = 0; i < divisions; i++) {
+    let element = document.createElement('div');
+    element.style.backgroundColor = 'red';
+    element.style.border = 'solid';
+    element.style.borderColor = 'blue'
+    element.style.top = '20px'
+    element.style.left = `${divisions}`
+    element.style.width = '20px';
+    element.style.height = '20px';
+    document.body.appendChild(element);
+}
